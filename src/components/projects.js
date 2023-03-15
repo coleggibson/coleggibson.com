7320fbbb534646b9cf9dcfc2becff02434a3b5b7
Cole Gibson <coleggibson@gmail.com>
2023-03-15 10:35:30 -0500

    added spoonacular api project

--- src/components/projects.js
+++ src/components/projects.js
@@ -15,6 +15,19 @@ const Projects = ({projectsSection, topFunction}) => {
             <div id='projects-container'>
                 {/* <div id='slideshow-container'></div> */}
                 <div className='project-item'> 
+                <div className="project-item-title">API Recipe Search (Currently Building)</div>
+                <div className='project-item-description'>Using <a href='spoonacular.com'>Spoonacular.com API</a></div>
+                    <img className='project-image' src={LIClone} alt='memorycard-screenshot'/>
+                    <div class='project-buttons'>
+                        <button class='button'>View Code</button>
+                        <a href="https://github.com/coleggibson/li-remade" target="_blank">
+                        </a>
+                        <a href="https://coleggibson.github.io/li-remade/" target="_blank">
+                        <button class='button'>Live Demo</button>
+                    </a>
+                    </div>
+                </div>
+                <div className='project-item'> 
                 <div className="project-item-title">LinkedIn Front Page</div>
                 <div className='project-item-description'>This is a recreation of the website we all know and love (sometimes), LinkedIn. It was created with React JS, Jekyll, and hosted on GitHub Pages.  I have grown to be a fan of the grid layout and had the opportunity to be able to flex those skills on this project for the overall body layout. This page is also friendly and adjustable for devices on smaller screens. I also recreated the top left search function that grays out the page on click.</div>
                     <img className='project-image' src={LIClone} alt='memorycard-screenshot'/>
