48b7a7f6b69f57bfb89260476ef17896a73b9051
Babagusto <coleggibson@gmail.com>
2023-07-31 18:58:16 -0500

    Pulled from main branch

--- src/components/about.js
+++ src/components/about.js
@@ -18,7 +18,11 @@ const About = ({aboutSection, contactSection}) => {
             <img id='headshot' src={headshot}/>
             <div id='right-side-content'>
                 <p id='about-content'>
+<<<<<<< HEAD
                     Front-End Developer based out of Nashville, TN with a lifelong passion for technology. Fluent in React, JS, HTML5, and CSS. Background in project management and able to work out a solution to any complex problem. When I'm not coding you can find me at the local arcade or attending the nearest festival with Tame Impala.
+=======
+                    Front-End Developer based out of Nashville, TN with a passion for technology. Background in project management and able to work out a solution to any complex problem. When I'm not coding you can find me at the local arcade or at nearest music festival.
+>>>>>>> c14a543 (Updated sizes)
                 </p>
             <button id='contact-button' onClick={() => scrollDown(contactSection)}>Let's build something together!</button>
             </div>
--- src/components/contact.js
+++ src/components/contact.js
@@ -10,6 +10,7 @@ const Contact= ({contactSection}) => {
     return (
         <div id='contact-section' ref={contactSection}>
             <div id='contact-container'>
+<<<<<<< HEAD
                 <div className='contact-item'>
                     <div>coleggibson@gmail.com</div>
                     <img id='gmail' src={Gmail} className='contact-img' alt='gmail'/>
@@ -26,6 +27,28 @@ const Contact= ({contactSection}) => {
                         </a>
                     </div>
                 </div>
+=======
+            <div className='contact-item'>
+            <div id='email-container'>coleggibson@gmail.com</div>
+            <img id='gmail' src={Gmail} className='contact-img' alt='gmail'/>
+            </div>
+            <div className='contact-container-bottom'>
+            <div className='contact-item'>
+            <a href="https://github.com/coleggibson" target="_blank">
+            <img src={GitHub} className='contact-img' alt='github'/>
+            </a>
+            </div>
+            <div className='contact-item'>
+            <a href="https://www.linkedin.com/in/colemanggibson/" target="_blank">
+            <img src={LinkedIn} className='contact-img' alt='linkedin'/>
+            </a>
+            </div>
+            </div>
+            {/* <div className='contact-item'>
+            <div>resume</div>
+            <div>image</div>
+            </div> */}
+>>>>>>> c14a543 (Updated sizes)
             </div>
         </div>
     )
